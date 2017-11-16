91a2dfa3003ee24ddd24bfb01db60bca6cf01abc
Viacheslav Lebedev <viacheslavCreator@yandex.ru>
2017-11-16 23:06:35 +0300

    стр.72 Постоянные свойства php school 4 урок дошел до php

---
+++ school_php_programming/index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!DOCTYPE HTML PUBLIC "">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Title</title>
+        <meta name="description" content="Уроки" >
+        <meta name="keywords" content="Уроки">
+        <link href="st.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <p>Абзац 1</p>
+        <?php
+            echo "Hello world!";
+        ?>
+        <p>Абзац последний</p>
+    </body>
+</html>
